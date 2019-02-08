7be51344dc8a54975d5a1f73e1d0a7f7f26fda12
Jean-Baptiste Weibel <jb.weibel@gmail.com>
2019-02-08 15:15:11 +0100

    cleanup bresenham

--- preprocessing/bresenham.cpp
+++ preprocessing/bresenham.cpp
@@ -19,9 +19,6 @@ void bresenham_line_low(int x0, int y0, int x1, int y1, std::vector<int> & min_y
   int D = 2*dy - dx;
   int y = y0;
 
-  // coords_x.reserve(x1-x0+1);
-  // coords_y.reserve(x1-x0+1);
-
   for (int x= x0; x<=x1; x++) {
     // "plot" the x,y value
     int x_idx = x - min_x;
@@ -53,9 +50,6 @@ void bresenham_line_high(int x0, int y0, int x1, int y1, std::vector<int> & min_
   int D = 2*dx - dy;
   int x = x0;
 
-  // coords_x.reserve(y1-y0+1);
-  // coords_y.reserve(y1-y0+1);
-
   for (int y=y0; y<=y1; y++) {
     // "plot" the x,y value
     int x_idx = x - min_x;
@@ -64,8 +58,6 @@ void bresenham_line_high(int x0, int y0, int x1, int y1, std::vector<int> & min_
 
     if (y < min_y[x_idx])
       min_y[x_idx] = y;
-    // coords_x.push_back(x);
-    // coords_y.push_back(y);
     if (D>0) {
       x += xi;
       D -= 2*dy;
@@ -89,75 +81,3 @@ void bresenham_line(int x0, int y0, int x1, int y1, std::vector<int> & min_y, st
     }
   }
 }
-
-// void bresenham_line_low(int x0, int y0, int x1, int y1, std::vector<int> & coords_x, std::vector<int> & coords_y) {
-//   int dx = x1 - x0;
-//   int dy = y1 - y0;
-//   int yi = 1;
-//   if (dy < 0) {
-//     yi = -1;
-//     dy = -dy;
-//   }
-
-//   int D = 2*dy - dx;
-//   int y = y0;
-
-//   coords_x.reserve(x1-x0+1);
-//   coords_y.reserve(x1-x0+1);
-
-//   for (int x= x0; x<=x1; x++) {
-//     // "plot" the x,y value
-//     coords_x.push_back(x);
-//     coords_y.push_back(y);
-//     if (D>0) {
-//       y += yi;
-//       D -= 2*dx;
-//     }
-//     D += 2*dy;
-//   }
-// }
-
-
-// void bresenham_line_high(int x0, int y0, int x1, int y1, std::vector<int> & coords_x, std::vector<int> & coords_y) {
-//   int dx = x1 - x0;
-//   int dy = y1 - y0;
-//   int xi = 1;
-
-//   if (dx < 0) {
-//     xi = -1;
-//     dx = -dx;
-//   }
-
-//   int D = 2*dx - dy;
-//   int x = x0;
-
-//   coords_x.reserve(y1-y0+1);
-//   coords_y.reserve(y1-y0+1);
-
-//   for (int y=y0; y<=y1; y++) {
-//     // "plot" the x,y value
-//     coords_x.push_back(x);
-//     coords_y.push_back(y);
-//     if (D>0) {
-//       x += xi;
-//       D -= 2*dy;
-//     }
-//     D += 2*dx;
-//   }
-// }
-
-// void bresenham_line(int x0, int y0, int x1, int y1, std::vector<int> & coords_x, std::vector<int> & coords_y) {
-//   if (abs(y1 - y0) < abs(x1 - x0)) {
-//     if (x0 > x1) {
-//       bresenham_line_low(x1, y1, x0, y0, coords_x, coords_y);
-//     } else {
-//       bresenham_line_low(x0, y0, x1, y1, coords_x, coords_y);
-//     }
-//   } else {
-//     if (y0 > y1) {
-//       bresenham_line_high(x1, y1, x0, y0, coords_x, coords_y);
-//     } else {
-//       bresenham_line_high(x0, y0, x1, y1, coords_x, coords_y);
-//     }
-//   }
-// }
